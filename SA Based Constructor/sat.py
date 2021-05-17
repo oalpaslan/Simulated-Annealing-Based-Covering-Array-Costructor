d3756f2aa6ac90c49599be821addd0d1621f9fca
oalpaslan <oalpaslan@sabanciuniv.edu>
2021-05-17 04:17:51 +0300

    XML input format

--- SA Based Constructor/sat.py
+++ SA Based Constructor/sat.py
@@ -12,7 +12,6 @@ import numpy as np
 from numpy import asarray
 from numpy import unique
 import xml.dom.minidom
-import CSP_Solver as CS
 import time
 import itertools
 from itertools import combinations
@@ -44,12 +43,16 @@ def bino_coef(n, k):  # Binomial coefficient function
         return 0
 
 
-def create_array(no_opts, no_vals):
+def create_array(mydict):
     arr = []
     # Takes the cartesian product of options with values. This will create a configuration settings list.
-    for i in product(np.arange(no_vals), repeat=no_opts):
-        arr.append(list(i))
+    for i in my_product(mydict):
+        arr.append(list(i.values()))
     return asarray(arr)  # This will return the arr with the numpy array form.
+
+
+def my_product(mydict):
+    return (dict(zip(mydict, x)) for x in product(*mydict.values()))
 
 
 def create_test_array(array, size):
@@ -110,6 +113,7 @@ def neighboring_state_gen(data, no_opts, no_vals, c_size):
 
 
 doc = xml.dom.minidom.parse(input('Please enter the name of the XML file: '))
+mydict = {}
 for node in doc.getElementsByTagName('Parameter'):
     if node.hasChildNodes():
         id = node.getAttribute('id')
@@ -117,76 +121,86 @@ for node in doc.getElementsByTagName('Parameter'):
         x = []
         for value in values:
             x += value.firstChild.nodeValue
-        print("id:%s, values:%s" % (id, x))
+        mydict[id] = x
     else:
         txt = node.getAttribute('text')
         print(txt)
-
-# t = int(input('Coverage strength(t): '))
+print(mydict)
+arr = create_array((mydict))
+print(arr)
+t = int(input('Coverage strength(t): '))
 # no_opts = int(input('Number of options: '))
 # no_vals = int(input('Number of option values for every option: '))
+count = 0
+value_count = 1
+for x in mydict:
+    for i in range(0, len(x)):
+        value_count *= x[i]
+print(count)
+print(len(mydict))
+no_opts = len(mydict)
 
-# starting_temp = 1
-# stopping_temp = 0.000001
-# running = 100
+starting_temp = 1
+stopping_temp = 0.000001
+running = 100
 
-# arr = create_array(no_opts, no_vals)
-# c_size = find_max_size(t, no_opts, no_vals)
-# working_test = create_test_array(arr, c_size)
-# # test = create_test_array(arr, c_size)
-# # miss = count_miss(test, t, no_vals)
-# # if (miss == 0):
-# #     best = test
-# # first_array = best
-# while (running > 0):
-#     test = create_test_array(arr, c_size)
-#     # check if all interactions are represented in this test array
-#     if (check_interactions(test, t, no_vals)):
-#         # if they are, reduce the covariance size and try again
-#         c_size = c_size - 1
-#         working_test = test
-#     else:
-#         # else, the iterations if we have a working test
-#         if (working_test.shape[0] != 0):
-#             running = running - 1
-# first_array = best = working_test
-# print("First array:\n", working_test)
-# print("Covering array size: ", working_test.shape[0], working_test.shape[1],
-#       " with the total of ", working_test.shape[0] * working_test.shape[1], " indices.")
-# print(". . .")
-# c_rate = cooling_rate(starting_temp, stopping_temp, t, no_opts)
-# temp = starting_temp
-# # working_test = create_test_array(test, c_size)
-# start = time.time()
-# while temp > stopping_temp:
-#     working_test = create_test_array(working_test, c_size)
-#     miss = count_miss(working_test, t, no_vals)
-#     if (miss == 0):
-#         best = working_test
-#         c_size -= 1
-#         temp = starting_temp
-#         continue
-#     neighbor = neighboring_state_gen(working_test, no_opts, no_vals, c_size)
-#     t_miss_n = count_miss(neighbor, t, no_vals)
-#     diff_miss = t_miss_n - miss
-#     if working_test.shape[0] == neighbor.shape[0] or (diff_miss < 0 or (random.randint(0, 1) < (math.e)**((-(scipy.constants.k))*diff_miss/temp))):
-#         working_test = neighbor
-#         if count_miss(working_test, t, no_vals) == 0:
-#             best = working_test
-#             c_size -= 1
-#             temp = starting_temp
-#             continue
-#     temp = temp - (temp * c_rate)
-# stop = time.time()
-# print(stop-start)
-# c_array = np.unique(best, axis=0)
-# if c_array.size < first_array.size:
-#     print("Final missing tuple count: ", count_miss(best, t, no_vals))
-#     print(c_array)
-#     print("Covering array size: ", c_array.shape[0], c_array.shape[1],
-#           " with the total of ", c_array.shape[0] * c_array.shape[1], " indices.")
-# else:
-#     print(first_array)
-#     print("Final missing tuple count: ", count_miss(first_array, t, no_vals))
-#     print("Covering array size: ", first_array.shape[0], first_array.shape[1],
-#           " with the total of ", first_array.shape[0] * first_array.shape[1], " indices.")
+#arr = create_array(no_opts, no_vals)
+c_size = arr.shape[0]
+working_test = create_test_array(arr, c_size)
+# test = create_test_array(arr, c_size)
+# miss = count_miss(test, t, no_vals)
+# if (miss == 0):
+#     best = test
+# first_array = best
+while (running > 0):
+    test = create_test_array(arr, c_size)
+    # check if all interactions are represented in this test array
+    if (check_interactions(test, t, no_vals)):
+        # if they are, reduce the covariance size and try again
+        c_size = c_size - 1
+        working_test = test
+    else:
+        # else, the iterations if we have a working test
+        if (working_test.shape[0] != 0):
+            running = running - 1
+first_array = best = working_test
+print("First array:\n", working_test)
+print("Covering array size: ", working_test.shape[0], working_test.shape[1],
+      " with the total of ", working_test.shape[0] * working_test.shape[1], " indices.")
+print(". . .")
+c_rate = cooling_rate(starting_temp, stopping_temp, t, no_opts)
+temp = starting_temp
+# working_test = create_test_array(test, c_size)
+start = time.time()
+while temp > stopping_temp:
+    working_test = create_test_array(working_test, c_size)
+    miss = count_miss(working_test, t, no_vals)
+    if (miss == 0):
+        best = working_test
+        c_size -= 1
+        temp = starting_temp
+        continue
+    neighbor = neighboring_state_gen(working_test, no_opts, no_vals, c_size)
+    t_miss_n = count_miss(neighbor, t, no_vals)
+    diff_miss = t_miss_n - miss
+    if working_test.shape[0] == neighbor.shape[0] or (diff_miss < 0 or (random.randint(0, 1) < (math.e)**((-(scipy.constants.k))*diff_miss/temp))):
+        working_test = neighbor
+        if count_miss(working_test, t, no_vals) == 0:
+            best = working_test
+            c_size -= 1
+            temp = starting_temp
+            continue
+    temp = temp - (temp * c_rate)
+stop = time.time()
+print(stop-start)
+c_array = np.unique(best, axis=0)
+if c_array.size < first_array.size:
+    print("Final missing tuple count: ", count_miss(best, t, no_vals))
+    print(c_array)
+    print("Covering array size: ", c_array.shape[0], c_array.shape[1],
+          " with the total of ", c_array.shape[0] * c_array.shape[1], " indices.")
+else:
+    print(first_array)
+    print("Final missing tuple count: ", count_miss(first_array, t, no_vals))
+    print("Covering array size: ", first_array.shape[0], first_array.shape[1],
+          " with the total of ", first_array.shape[0] * first_array.shape[1], " indices.")
